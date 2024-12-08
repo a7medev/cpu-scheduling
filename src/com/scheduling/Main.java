e3e5dcc1582bbf6a4c0bd0c9c7b380eb056b34d5
Mahmoud <mahmoudsamymahmoud01@gmail.com>
2024-12-09 00:38:48 +0200

    chore: remove some comments

--- src/com/scheduling/Main.java
+++ src/com/scheduling/Main.java
@@ -16,23 +16,6 @@ public class Main {
                 new Process("P3", 4, 10, 3, 5, null),
                 new Process("P4", 29, 4, 10, 2, null)
         );
-        //SJF
-//        P0 0
-//        P1 6
-//        P2 8
-//        P3 7
-//        P4 3
-
-        //SRTF
-//        P1 0 7
-//        P2 2 4
-//        P3 4 1
-//        P4 5 4
-
-//        P1 17 0 4 4  6+[0/2.9] +[17/1.7]=16
-//        P2 6  3 9 3 1+[3/2.9]+[6/1.7]=7
-//        P3 10 4 3 5  7+[4/2.9]+[10/1.7]=15
-//        P4 4 29 10 2 0+[29/2.9]+[4/1.7]=13
 
         Statistics statistics = new Statistics();
         Logger logger = new Logger();
